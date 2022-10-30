57c34217e179f2bcbd446dad81a08600fbdac5cd
phuong <phuongdenpro@gmail.com>
2022-10-31 00:12:07 +0700

    first commit

---
+++ chuyenbay-service/src/main/java/com/example/chuyenbayservice/ChuyenbayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.chuyenbayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuyenbayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuyenbayServiceApplication.class, args);
+	}
+
+}
---
+++ chuyenbay-service/src/test/java/com/example/chuyenbayservice/ChuyenbayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chuyenbayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuyenbayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
